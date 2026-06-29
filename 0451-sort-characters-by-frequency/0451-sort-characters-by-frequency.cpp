9533e24b39c2190a6cd6cafb0891a40db2388c9a
Prashik Wasnik <wasnikprashik54@gmail.com>
2026-06-30 00:00:33 +0530

    Time: 0 ms (100%), Space: 11 MB (70.25%) - LeetHub

---
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -0,0 +1,30 @@
+bool cmp(pair<char,int>p1,pair<char,int>p2){
+    if(p1.second>p2.second) return true;
+    return false;
+}
+class Solution {
+public:
+    string frequencySort(string s) {
+        int n=s.size();
+        unordered_map<char,int>mp;
+        for(char ch: s){
+            mp[ch]++;
+        }
+
+        vector<pair<char,int>>p;
+        for(auto ele : mp){
+            p.push_back(ele);
+        }
+
+        sort(p.begin(),p.end(),cmp);
+
+        string ans="";
+        for(auto ele :p){
+            for(int i=0;i<ele.second;i++){
+                ans+=ele.first;
+            }
+        }
+
+        return ans;
+    }
+};
