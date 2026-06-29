262a65109d4a588282becc3ef12053858541d655
Prashik Wasnik <wasnikprashik54@gmail.com>
2026-06-30 00:01:07 +0530

    Time: 7 ms (12.63%), Space: 15.4 MB (14.68%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
@@ -2,7 +2,7 @@ bool cmp(pair<int,int> p1 ,pair<int,int>p2){
     if(p1.second==p2.second){
         return p1.first>p2.first;
     }
-    return p1.second<p2.second;
+    return p1.second<p2.second; //asending sort by frequency
 }
 class Solution {
 public:
