30d378c20effcf489827d3aaf5116c4f2b027561
Prashik Wasnik <wasnikprashik54@gmail.com>
2026-06-30 22:47:22 +0530

    Time: 0 ms (100%), Space: 115.6 MB (67.68%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
@@ -8,13 +8,14 @@ public:
         int num3=INT_MAX;
         for(int i=0;i<n;i++){
             num3=nums[i];
-            if(num1<num2 && num2<num3) return true;
+            // if(num1<num2 && num2<num3) return true;
             if(num1>=num3){
                 num1=num3;
             }
             else if(num2>=num3){
                 num2=num3;
             }
+            else return true;
             
         }
         return false;
