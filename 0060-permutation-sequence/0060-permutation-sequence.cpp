3f5f86e25325fa1f69f56c9265505f8dbf541e13
Prashik Wasnik <wasnikprashik54@gmail.com>
2026-07-13 00:07:41 +0530

    Time: 115 ms (10.43%), Space: 8.8 MB (36.49%) - LeetHub

---
+++ 0060-permutation-sequence/0060-permutation-sequence.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string getPermutation(int n, int k) {
+        vector<int>v;
+        for(int i=0;i<n;i++){
+            v.push_back(i+1);
+        }
+        int count=1;
+        string ans="";
+        if(count==k){
+            for(int i=0;i<v.size();i++){
+                ans+=to_string(v[i]);
+            }
+            return ans;
+        }
+        while(next_permutation(v.begin(),v.end())){
+            count++;
+            if(count==k){
+                for(int i=0;i<v.size();i++){
+                    ans+=to_string(v[i]);
+                }
+            }
+        }
+        return ans;
+    }
+};
