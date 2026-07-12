355799bc65019e0895fe1e56262fd3c76af7013a
Prashik Wasnik <wasnikprashik54@gmail.com>
2026-07-13 00:25:54 +0530

    Time: 51 ms (19.94%), Space: 8.5 MB (76.37%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.cpp
+++ 0060-permutation-sequence/0060-permutation-sequence.cpp
@@ -4,26 +4,28 @@ public:
         
         //using inbuilt next permutation function (no recursion)
 
-        vector<int>v;
+        // vector<int>v;
+        string s = "";
         for(int i=0;i<n;i++){
-            v.push_back(i+1);
+            s+=to_string(i+1);
         }
+
+        
         int count=1;
-        string ans="";
-        if(count==k){
-            for(int i=0;i<v.size();i++){
-                ans+=to_string(v[i]);
-            }
-            return ans;
-        }
-        while(next_permutation(v.begin(),v.end())){
+        if(k==1) return s;
+        //string ans="";
+        // if(count==k){
+        //     for(int i=0;i<s.size();i++){
+        //         ans+=to_string(s[i]);
+        //     }
+        //     return ans;
+        // }
+        while(next_permutation(s.begin(),s.end())){
             count++;
             if(count==k){
-                for(int i=0;i<v.size();i++){
-                    ans+=to_string(v[i]);
-                }
+                return s;
             }
         }
-        return ans;
+        return s;
     }
 };
