e781b37e196ed3719b34cddb735aff7aefd8917a
Prashik Wasnik <wasnikprashik54@gmail.com>
2026-07-20 19:18:23 +0530

    Time: 8 ms (82.86%), Space: 11.9 MB (54.3%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -9,15 +9,31 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        ListNode* temp = head;
-        unordered_set<ListNode*>st;
-        while(temp!=NULL){
-            if(st.find(temp) != st.end()){
-                return true;
-            }
-            st.insert(temp);
-            temp = temp->next;
+        //m2 - Floyd's Cycle-Finding Algorithm
+        //using fast and slow pointer
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while(fast!=NULL && fast->next != NULL){
+           
+           slow = slow->next;
+           fast = fast->next->next; 
+           if(slow==fast) return true;
         }
         return false;
+
+
+
+        // //m1- using set  TC-(n) , sc - (n)
+        // ListNode* temp = head;
+        // unordered_set<ListNode*>st;
+        // while(temp!=NULL){
+        //     if(st.find(temp) != st.end()){
+        //         return true;
+        //     }
+        //     st.insert(temp);
+        //     temp = temp->next;
+        // }
+        // return false;
     }
 };
