23e1910a1cb67e8ca9260fa2bcda31b5ea6cbddf
Prashik Wasnik <wasnikprashik54@gmail.com>
2026-07-20 21:51:40 +0530

    Time: 7 ms (66.73%), Space: 11.4 MB (24.45%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -9,15 +9,40 @@
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-        ListNode* temp = head;
-        unordered_set<ListNode*>s;
-        while(temp!=NULL){
-            if(s.find(temp)!=s.end()){
-                return temp;
+        //m2
+        ListNode* fast = head;
+        ListNode* slow = head;
+        bool cycle =false;
+        while(fast!=NULL && fast->next!=NULL){
+            fast = fast->next->next;
+            slow = slow->next;
+            if(fast==slow){
+                cycle = true;
+                break;
             }
-            s.insert(temp);
-            temp = temp->next;
         }
+        if(cycle==false) return NULL;
+
+        ListNode* temp = head;
+        while(temp!=NULL){
+            if(temp==slow) return temp;
+            temp = temp->next;
+            slow = slow->next;
+        }
+
         return NULL;
+        
+
+        // //m1 - using set
+        // ListNode* temp = head;
+        // unordered_set<ListNode*>s;
+        // while(temp!=NULL){
+        //     if(s.find(temp)!=s.end()){
+        //         return temp;
+        //     }
+        //     s.insert(temp);
+        //     temp = temp->next;
+        // }
+        // return NULL;
     }
 };
