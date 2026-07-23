02b79c12db07b5db5d1d4cac85b2e72fd053a51b
Prashik Wasnik <wasnikprashik54@gmail.com>
2026-07-23 21:50:29 +0530

    Time: 0 ms (100%), Space: 199 MB (28.13%) - LeetHub

--- 3513-number-of-unique-xor-triplets-i/3513-number-of-unique-xor-triplets-i.cpp
+++ 3513-number-of-unique-xor-triplets-i/3513-number-of-unique-xor-triplets-i.cpp
@@ -1,15 +1,28 @@
 class Solution {
 public:
     int uniqueXorTriplets(vector<int>& nums) {
+        // Get the total number of elements in the input vector
         int n = nums.size();
-        if(n==1) return 1;
-        if(n==2) return 2;
+        
+        // Base case: If there is only 1 element, it can form 1 unique configuration
+        if(n == 1) return 1;
+        
+        // Base case: If there are 2 elements, they can form 2 unique configurations
+        if(n == 2) return 2;
+        
         int temp;
-        int i =0;
-        while(n>=temp){ //temp is for 2 prower
-            temp = pow(2,i);
-            i++;
+        int i = 0;
+        
+        // Find the smallest power of 2 that is strictly greater than n
+        while(n >= temp) { 
+            // Calculate 2 raised to the power of i (2^i)
+            temp = pow(2, i); 
+            
+            // Increment the exponent for the next iteration
+            i++; 
         }
-        return temp;
+        
+        // Return the calculated power of 2
+        return temp; 
     }
 };
