c2fa84684f0040c76844fe49de5b08ee8c5abb01
Prashik Wasnik <wasnikprashik54@gmail.com>
2026-07-23 22:32:33 +0530

    Time: 20 ms (0.24%), Space: 28.6 MB (16.84%) - LeetHub

---
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
@@ -0,0 +1,55 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int levels(TreeNode* root){
+        if(root == NULL) return 0;
+        return 1 + max(levels(root->left),levels(root->right));
+    }
+    vector<int> leftOrder(TreeNode* root, int n, vector<int>&temp){
+        if(root==NULL) return temp; //no need to print
+        if(n==1){
+            temp.push_back(root->val);
+        }
+        leftOrder(root->left,n-1,temp); //left call
+        leftOrder(root->right,n-1,temp); //right call
+        return temp;
+    }
+    vector<int> rightOrder(TreeNode* root, int n, vector<int>&temp){
+        if(root==NULL) return temp ; //no need to print
+        if(n==1){
+            temp.push_back(root->val);
+        }
+        rightOrder(root->right,n-1,temp); //right call
+        rightOrder(root->left,n-1,temp); //left call
+        return temp;
+        
+    }
+    void traversal(TreeNode* root, int l, vector<vector<int>>&ans){
+        for(int i = 1;i<=l;i++){
+            vector<int>temp;
+            if(i%2!=0){ //odd ->simple order
+                leftOrder(root,i,temp);
+            }
+            else{ //even ->reverse order
+                rightOrder(root,i,temp);
+            }
+            ans.push_back(temp);
+        }
+    }
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        int l =levels(root);
+        vector<vector<int>>ans;
+        traversal(root,l,ans);
+        return ans;
+    }
+};
