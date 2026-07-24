489dc3b6f77de28e514c187b6463db5a60b16677
Prashik Wasnik <wasnikprashik54@gmail.com>
2026-07-24 19:08:20 +0530

    Time: 2202 ms (22.47%), Space: 98.4 MB (39.89%) - LeetHub

--- 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.cpp
+++ 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.cpp
@@ -6,7 +6,7 @@ public:
 
         unordered_set<int>pairXor;
         for(int i = 0; i<n ; i++){
-            for(int j = 0; j < n; j++){
+            for(int j = i; j < n; j++){
                 pairXor.insert(nums[i]^nums[j]);
             }
         }
