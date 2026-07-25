c5da0b02a7bc27447ce49a4eb8334482cfe1e7e0
Prashik Wasnik <wasnikprashik54@gmail.com>
2026-07-25 09:08:56 +0530

    Time: 3 ms (15.04%), Space: 9.4 MB (6.58%) - LeetHub

---
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int maxProduct(int n) {
+        string s = to_string(n);
+        sort(s.begin(),s.end());
+        int len = s.size();
+
+        return (s[len-1]-'0')*(s[len-2]-'0');
+    }
+};
