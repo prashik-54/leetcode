252416d10000ef1056182e699d2696fd8f4bedaf
Prashik Wasnik <wasnikprashik54@gmail.com>
2026-09-08 14:40:59 +0530

    Time: 0 ms (100%), Space: 8.6 MB (51.38%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.cpp
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -1,9 +1,13 @@
 class Solution {
 public:
     int countCommas(int n) {
-        string s = to_string(n);
-        int len = s.size();
-        if(len<4) return 0;
+        //m1
+        // string s = to_string(n);
+        // int len = s.size();
+        // if(len<4) return 0;
+        // return n-1000+1;
+
+        if(n<1000) return 0;
         return n-1000+1;
 
     }
