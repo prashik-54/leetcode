6890eaffac82965b72c464b9ba5b38163706e834
Prashik Wasnik <wasnikprashik54@gmail.com>
2026-09-13 02:11:57 +0530

    Time: 0 ms (100%), Space: 8.3 MB (11.16%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -1,6 +1,14 @@
 class Solution {
 public:
+    int solve(int n){
+        int count = 0;
+        while(n){
+            if(n & 1 == 1) count++;
+            n>>=1;
+        }
+        return count;
+    }
     int hammingWeight(int n) {
-        return __builtin_popcount(n);
+        return solve(n);
     }
 };
